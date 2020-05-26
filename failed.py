defa8a4cf8a71aaf815b0ae67683be9b440b01ca
RachnaG <53226805+RachnaG@users.noreply.github.com>
2020-05-27 02:09:30 +0530

    Update failed.py

--- failed.py
+++ failed.py
@@ -1,8 +1,8 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-host_address = "9d8e7540a04f8a"
-host_pass = "d788d7303e7936"
+host_address = "19ee3e2137d7f1"
+host_pass = "61df57bfaa2a04"
 guest_address = "shubhamkhandelwal523@gmail.com"
 subject = "Regarding failure of main.py"
 content = '''Hello, 
@@ -13,7 +13,7 @@ message['From'] = host_address
 message['To'] = guest_address
 message['Subject'] = subject
 message.attach(MIMEText(content, 'plain'))
-session = smtplib.SMTP('smtp.mailtrap.io', 587)
+session = smtplib.SMTP('smtp.mailtrap.io', 2525)
 session.starttls()
 session.login(host_address, host_pass)
 text = message.as_string()
