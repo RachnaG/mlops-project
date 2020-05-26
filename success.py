e92da15cfceb3c8b3e0af567d34ca8257f3a5cd2
RachnaG <53226805+RachnaG@users.noreply.github.com>
2020-05-27 02:07:46 +0530

    Update success.py

--- success.py
+++ success.py
@@ -1,9 +1,9 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-host_address = "9d8e7540a04f8a"
-host_pass = "d788d7303e7936"
-guest_address = "shubhamkhandelwal523@gmail.com"
+host_address = "19ee3e2137d7f1"
+host_pass = "61df57bfaa2a04"
+guest_address = "rachnakgupta81@gmail.com"
 subject = "Regarding Success of your model "
 content = '''Hello, 
 				Developer used the commit and it is successfully run . The mail is sent No failed comes up
@@ -13,7 +13,7 @@ message['From'] = host_address
 message['To'] = guest_address
 message['Subject'] = subject
 message.attach(MIMEText(content, 'plain'))
-session = smtplib.SMTP('smtp.mailtrap.io', 587)
+session = smtplib.SMTP('smtp.mailtrap.io', 2525)
 session.starttls()
 session.login(host_address, host_pass)
 text = message.as_string()
